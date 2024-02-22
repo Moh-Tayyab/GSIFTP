b9e6751db5bcf5aeac3e27d1c97cfa388916b87b
Muhammad-Tayyab <160000753+Moh-Tayyab@users.noreply.github.com>
2024-02-22 09:56:10 +0500

    Envirment Setup and Installation

---
+++ a_installation.ts
@@ -0,0 +1,19 @@
+/*
+1.install Node.js
+((https://nodejs.org/en))
+commands after installing node.js
+node -v 
+npm -v
+
+2.Git
+((https://nodejs.org/en))
+command prompt:
+git -v
+3.Typescript
+((https://nodejs.org/en))
+command prompt:
+npm insall -g typescript@latest
+tsc -v
+4.Vs code
+((https://nodejs.org/en))
+*/
